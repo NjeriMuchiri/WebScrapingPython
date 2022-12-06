a928b592624751b7fc5745558958e384da2e2f37
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-06 14:36:02 +0300

    Added error handling

--- scrapetest.py
+++ scrapetest.py
@@ -1,6 +1,15 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+from urllib.error import URLError
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.h1)
+try:
+   html = urlopen("http://pythonscraping.com/pages/page1.html")
+   bs = BeautifulSoup(html.read(), 'html.parser')
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print("The server could not be found")
+else:
+    print(bs.h1)
+
