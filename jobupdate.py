c1ce995860925dfc321ae43667100215dfcc3365
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-07 15:42:12 +0300

    Added a replace method

--- jobupdate.py
+++ jobupdate.py
@@ -5,5 +5,5 @@ html__text = requests.get('https://www.timesjobs.com/candidate/job-search.html?s
 # print(html__text)
 soup = BeautifulSoup(html__text.text, 'lxml')
 job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_ = 'joblist-comp-name').get_text()
+company_name = job.find('h3', class_ = 'joblist-comp-name').get_text().replace(' ', '')
 print(company_name)
