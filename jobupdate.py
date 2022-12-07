21bd60c415fb5f44a80ff7713e21594fa2b38609
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-07 14:45:55 +0300

    Added the new code scrapes

---
+++ jobupdate.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+html__text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
+# print(html__text)
+soup = BeautifulSoup(html__text.text, 'lxml')
+job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+company_name = job.find('h3', class_ = 'joblist-comp-name')
+print(job.prettify())
