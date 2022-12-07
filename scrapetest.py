1aac038ddf4988d4d8e0734e194a35d13da4ce8f
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-07 10:20:22 +0300

    Just adding some new scraps

--- scrapetest.py
+++ scrapetest.py
@@ -4,8 +4,10 @@ from urllib.error import HTTPError
 from urllib.error import URLError
 
 try:
-   html = urlopen("http://pythonscraping.com/pages/page1.html")
+   html = urlopen("http://pythonscraping.com/pages/page3.html")
    bs = BeautifulSoup(html.read(), 'html.parser')
+   for sibling in bs.find('table',{'id':'giftList'}).tr.next_siblings:
+        print(sibling)                         
 except HTTPError as e:
     print(e)
 except URLError as e:
