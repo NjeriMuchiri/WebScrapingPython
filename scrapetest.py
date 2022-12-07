3fb2b5b98f67b0d9f47412fafa01436574007ec4
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-07 11:39:11 +0300

    Added a new method

--- scrapetest.py
+++ scrapetest.py
@@ -6,12 +6,13 @@ from urllib.error import URLError
 try:
    html = urlopen("http://pythonscraping.com/pages/page3.html")
    bs = BeautifulSoup(html.read(), 'html.parser')
-   for sibling in bs.find('table',{'id':'giftList'}).tr.next_siblings:
-        print(sibling)                         
+   print(bs.find('img', {'src': '../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
+   """ for sibling in bs.find('table',{'id':'giftList'}).tr.next_siblings:
+        print(sibling) """                         
 except HTTPError as e:
     print(e)
 except URLError as e:
     print("The server could not be found")
 else:
-    print(bs.h1)
+    print(bs.h1.prettify())
 
